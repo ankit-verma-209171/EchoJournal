e9871c6c28854cc236ce69716405f156310d2fc3
Ankit Verma <ankitverma209171@gmail.com>
2025-01-18 22:11:38 +0530

    docs: add docs to application class

--- app/src/main/java/com/codeitsolo/echojournal/EchoJournalApplication.kt
+++ app/src/main/java/com/codeitsolo/echojournal/EchoJournalApplication.kt
@@ -3,5 +3,8 @@ package com.codeitsolo.echojournal
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Represents the android application.
+ */
 @HiltAndroidApp
 class EchoJournalApplication : Application()
