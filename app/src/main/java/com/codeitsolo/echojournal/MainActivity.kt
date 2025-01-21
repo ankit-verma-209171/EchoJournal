184f80a0b3d5a72ffafd67cd4cc6c28c70e96ee5
ankit.verma <ankitverma209171@gmail.com>
2025-01-21 08:54:52 +0530

    feat: update status and navigation bar colors

--- app/src/main/java/com/codeitsolo/echojournal/MainActivity.kt
+++ app/src/main/java/com/codeitsolo/echojournal/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.codeitsolo.echojournal
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -16,7 +18,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            // Currently, only light mode is supported
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT,
+            ),
+            // Currently, only light mode is supported
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT,
+            ),
+        )
         setContent {
             EchoJournalTheme {
                 EchoJournalApp()
